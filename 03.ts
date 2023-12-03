b980781480d862b28018b27049701c41e8a3220d
AJ Coppa <aj.coppa@gmail.com>
2023-12-03 15:03:45 -0500

    3*

--- 03.ts
+++ 03.ts
@@ -4,7 +4,7 @@ import { charIsNumeric, loadFromFile, sum } from "./lib";
 import { Coord, getAdjacentCoords, inBounds } from "./lib/Coord";
 
 async function main() {
-  const lines: string[] = await loadFromFile("03-example.txt");
+  const lines: string[] = await loadFromFile("03-input.txt");
   console.log(`Part 1: ${partOne(lines)}`);
 }
 
@@ -13,53 +13,48 @@ function partOne(lines: string[]): number {
 }
 
 function parseGrid(lines: string[]): number[] {
-  return parseGridHelper(lines, {x: 0, y: 0}, "", false, []);
-}
+  let coord = {x: 0, y: 0},
+      currentVal = "",
+      currentSymbolAdjacent = false,
+      accum: number[] = [];
+  
+  while (true) {
+    const value = lines[coord.y][coord.x];
+    let nextCurrentVal: string, nextSymbolAdjacent: boolean;
+    if (charIsNumeric(value)) {
+      // numeric, so concat to currentVal and check adjacency
+      nextCurrentVal = currentVal + value;
+      nextSymbolAdjacent =
+        currentSymbolAdjacent || coordIsSymbolAdjacent(lines, coord);
+    } else {
+      // non-numeric, so look at currentVal + symbol adjacency to determine whether to include
+      if (currentVal.length > 0 && currentSymbolAdjacent) {
+        accum.push(parseInt(currentVal, 10));
+      }
 
-function parseGridHelper(
-  lines: string[],
-  coord: Coord,
-  currentVal: string,
-  currentIsSymbolAdjacent: boolean,
-  accum: number[],
-): number[] {
-  const value = lines[coord.y][coord.x];
-  let nextCurrentVal: string, nextSymbolAdjacent: boolean;
-  if (charIsNumeric(value)) {
-    // numeric, so concat to currentVal and check adjacency
-    nextCurrentVal = currentVal + value;
-    nextSymbolAdjacent = currentIsSymbolAdjacent || coordIsSymbolAdjacent(lines, coord);
-  } else {
-    // non-numeric, so look at currentVal + symbol adjacency to determine whether to include
-    if (currentVal.length > 0 && currentIsSymbolAdjacent) {
-      accum.push(parseInt(currentVal, 10));
+      // reset currentVal + symbol adjacency
+      nextCurrentVal = "";
+      nextSymbolAdjacent = false;
     }
 
-    // reset currentVal + symbol adjacency
-    nextCurrentVal = "";
-    nextSymbolAdjacent = false;
-  }
-
-  let nextX = coord.x + 1;
-  let nextY = coord.y;
-  if (!inBounds(nextX, coord.y, lines[0].length, lines.length)) {
-    // move to the next row
-    nextX = 0;
-    nextY = coord.y + 1;
-    if (!inBounds(nextX, nextY, lines[0].length, lines.length)) {
-      // finished the whole grid, return our accumulated list of numbers
-      return accum;
+    let nextX = coord.x + 1;
+    let nextY = coord.y;
+    if (!inBounds(nextX, coord.y, lines[0].length, lines.length)) {
+      // move to the next row
+      nextX = 0;
+      nextY = coord.y + 1;
+      if (!inBounds(nextX, nextY, lines[0].length, lines.length)) {
+        // finished the whole grid, return our accumulated list of numbers
+        return accum;
+      }
     }
-  }
 
-  const nextCoord = {x: nextX, y: nextY};
-  return parseGridHelper(
-    lines,
-    nextCoord,
-    nextCurrentVal,
-    nextSymbolAdjacent,
-    accum
-  );
+    const nextCoord = { x: nextX, y: nextY };
+
+    coord = nextCoord;
+    currentVal = nextCurrentVal;
+    currentSymbolAdjacent = nextSymbolAdjacent;
+  }
 }
 
 function coordIsSymbolAdjacent(lines: string[], coord: Coord): boolean {
